74a469e7c4f6dc88f6d26725477211fa2aede1cd
Mister M <112970459+mister-Marco@users.noreply.github.com>
2024-03-27 11:03:01 -0300

    27/03/2024

--- main.js
+++ main.js
@@ -6,8 +6,31 @@ for(let i=0;i <botoes.length;i++){
         
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=60;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+
 }
