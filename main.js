e22652d3852ab4e1e96920f07e6369698a27a4a1
Mister M <112970459+mister-Marco@users.noreply.github.com>
2024-04-03 11:04:09 -0300

    03/04/2024

--- main.js
+++ main.js
@@ -16,8 +16,14 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-16T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+
+
+
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -29,8 +35,25 @@ function calculaTempo(tempoObjetivo){
 
     segundos %=60;
     minutos %=60;
-    horas %=60;
+    horas %=24;
+    if(tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "Prazo Finalizado";
+    }
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 
 }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+        }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+//comecaCronometro()
