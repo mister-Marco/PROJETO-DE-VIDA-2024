500528f7193a6acac210f652c22c6c9fc877c217
Mister M <112970459+mister-Marco@users.noreply.github.com>
2024-04-10 11:05:14 -0300

    10/04/2024

--- main.js
+++ main.js
@@ -37,17 +37,21 @@ function calculaTempo(tempoObjetivo){
     minutos %=60;
     horas %=24;
     if(tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 
 
 }
 
 function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById ("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById ("horas0").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById ("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById ("seg0").textContent = calculaTempo(tempos[1])[3];
+   for (let i=0; i<contadores.length;i++){
+       // contadores[i].textContent = calculaTempo(tempos[i]);
         }
 }
 
@@ -56,4 +60,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//comecaCronometro()
+comecaCronometro()
